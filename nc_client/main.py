35d28b3402febd6ef92c43181519e3f0d8118196
dan cohen <112656349+danc796@users.noreply.github.com>
2025-04-28 12:09:39 +0300

    Add files via upload

---
+++ nc_client/main.py
@@ -0,0 +1,11 @@
+from nc_client.ui.main_window import MainWindow
+
+def main():
+    app = MainWindow()
+    app.mainloop()
+
+
+#need to add a server code here
+
+if __name__ == "__main__":
+    main()
